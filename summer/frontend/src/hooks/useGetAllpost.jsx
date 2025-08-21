90de4b60ed14d8c2ae1839ff4110ec5778f87702
himanshu <himanshupandar1010@gmail.com>
2025-08-21 23:23:45 +0530

    update more apis

--- summer/frontend/src/hooks/useGetAllpost.jsx
+++ summer/frontend/src/hooks/useGetAllpost.jsx
@@ -2,14 +2,14 @@ import { setPosts } from "@/redux/postSlice";
 import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-
+import API_BASE from '@/confige';
 
 const useGetAllPost = () => {
     const dispatch = useDispatch();
     useEffect(() => {
         const fetchAllPost = async () => {
             try {
-                const res = await axios.get('http://localhost:3000/api/v1/post/all', { withCredentials: true });
+                const res = await axios.get(`${API_BASE}/api/v1/post/all`, { withCredentials: true });
                 if (res.data.success) { 
                     console.log(res.data.posts);
                     dispatch(setPosts(res.data.posts));
